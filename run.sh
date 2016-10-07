700c47109dac358001b89247c99d802d751a2586
William Lewis <william.lewis@lyst.com>
2016-10-07 15:13:23 +0100

    allow flow tmp dir to be overriden

--- run.sh
+++ run.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+TMP_DIR=${TMP_DIR:-/tmp/flow}
+
+echo "Using --temp-dir=${TMP_DIR}"
+
 while [ true ]
 do
     rsync -rpt /flow-src/* /usr/src/app/
     sleep 0.5
 done &
 
-flow server --temp-dir /flow
+while [ true ]
+do
+    flow server --temp-dir ${TMP_DIR}
+    sleep 2
+done
